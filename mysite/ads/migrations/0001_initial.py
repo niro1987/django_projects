18927fae5ae3a0cb50c2d276dfce81fa70ed39eb
Niels Perfors <niels.perfors1987@gmail.com>
2024-01-04 11:45:34 +0000

    db

--- mysite/ads/migrations/0001_initial.py
+++ mysite/ads/migrations/0001_initial.py
@@ -12,9 +12,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        (
-            "taggit",
-        ),
     ]
 
     operations = [
